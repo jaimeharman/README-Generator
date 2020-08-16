3fb380f90dabd2ced058e3598d4b61fc95a9140f
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-08-16 15:36:42 -0400

    Adding pseudocode

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,10 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
 
 // array of questions for user
 const questions = [
   "Enter a title",
   "Enter a description", 
-  "Enter a table of contents",
   "Enter an installation",
   "Enter a usage",
   "Enter a license",
@@ -15,6 +15,27 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+  console.log(data)
+
+  // Turn data.license into a badge
+
+  // Create a Description section
+
+  // Create a Table of Contents section: containing each section below as a hyperlink
+
+  // Create an Installation section
+
+  // Create a Usage section
+
+  // Create a Contributing section
+
+  // Create a License section
+
+  // Create a Tests section
+
+  // Create a Questions section: link to gitHub profile, instructions on how to contact
+
+  // Create the file with fs.writeFile
 }
 
 // function to initialize program
@@ -35,40 +56,40 @@ function init() {
       {
         type: 'input',
         message: questions[2],
-        name: 'tableOfContents'
-      },
-      {
-        type: 'input',
-        message: questions[3],
         name: 'installation'
       },
       {
         type: 'input',
-        message: questions[4],
+        message: questions[3],
         name: 'usage'
+      },
+      {
+        type: 'input', // Checkbox to select gitHub license options
+        message: questions[4],
+        name: 'license' 
       },
       {
         type: 'input',
         message: questions[5],
-        name: 'license'
-      },
-      {
-        type: 'input',
-        message: questions[6],
         name: 'contributors'
       },
       {
         type: 'input',
-        message: questions[7],
+        message: questions[6],
         name: 'tests'
       },
       {
         type: 'input',
-        message: questions[8],
+        message: questions[7],
         name: 'questions'
       }
+
+      // Ask for author email
     ])
-    .then((answers) => console.log(JSON.stringify(answers, null, '  ')));
+    .then((answers) => {
+      const data = JSON.stringify(answers, null, '  ')
+      writeToFile("README.md", data)
+    });
 }
 
 // function call to initialize program
